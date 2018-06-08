3de2b158f5dc73c57c305be7cbac5bf826ab885f
ddxiao <809660773@qq.com>
2018-06-08 17:23:52 +0800

    添加 BST类中的插入方法

--- myAlgorithm/src/binary_Search/BST.java
+++ myAlgorithm/src/binary_Search/BST.java
@@ -35,6 +35,30 @@ public class BST<Key extends Comparable<Key>, Value> {
 		return count == 0;
 	}
 	
+	// 向二叉树中插入一个新的（key，value）数据对
+	public void insert(Key key, Value value){
+		root = insert(root, key, value);
+	}
+	
+	
+	private Node insert(Node node, Key key, Value value){
+		if (node == null){	//树空的情况下，直接插入
+			count++;
+			return new Node(key, value);
+		}
+		
+		if(key.compareTo(node.key)==0){	//键和当前节点键相同，覆盖当前节点的值
+			node.value = value;
+		}else if(key.compareTo(node.key)<0){	//键小于当前节点键，递归左子树
+			node.left = insert(node.left, key, value);
+		}else{	//键小于当前节点键，递归右子树
+			node.right = insert(node.right, key, value);
+		}
+		
+		return node;
+	}
+	
+	
 	public static void mian(String[] args){
 		
 	}
